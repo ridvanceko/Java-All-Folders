f98bf2a9ae1a06ea052ffbef04982e6d66623db7
Ridvan Cekmez <ridvanceko@gmail.com>
2021-01-03 12:19:10 -0700

    ArrayList-Method

---
+++ src/ArrayListPractice/Methods.java
@@ -0,0 +1,16 @@
+package ArrayListPractice;
+
+import java.util.ArrayList;
+
+public class Methods {
+    public static void main(String[] args) {
+
+        //if you do not give the data type, by default it will be Object data type
+
+        ArrayList items = new ArrayList();
+        items.add("apple");
+        items.add("banana");
+        items.add(Boolean.TRUE);
+        items.add(false);
+    }
+}
