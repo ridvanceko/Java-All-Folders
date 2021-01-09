f7481aa9839533a64c6695b7683664866e5f1715
Ridvan Cekmez <ridvanceko@gmail.com>
2021-01-09 10:11:40 -0700

    HashSet-Iteration

--- src/Sets/HashSetPractice.java
+++ src/Sets/HashSetPractice.java
@@ -21,6 +21,53 @@ public class HashSetPractice {
 
         System.out.println(setOfStrings); //Sets (specifically HashSet, there are exception)  don't hold the order
 
-        
+        //checking if SEt run duplicate
+        setOfStrings.add("Banana");
+        System.out.println(setOfStrings);
+
+
+        // setOfStrings.clear(); - clears the Set
+
+        if(setOfStrings.contains("Raspberry"))
+            System.out.println("The set contains Raspberry");
+        else
+            System.out.println("It doesnt contains Raspberry");
+        setOfStrings.remove("Banana");
+
+        System.out.println("Banana is removed: " + setOfStrings);
+
+        System.out.println("The size of set: " + setOfStrings.size());
+
+        list.add("Banana");
+        list.add("Strawberry");
+        list.add("Raspberry");
+        list.add("Blueberry");
+        list.add("Blackberry");
+
+        System.out.println("This is our ArrayList: " + list);
+
+        HashSet<String> set4 = new HashSet<>(list); //converting the ArrayList to HashSet
+        System.out.println("This is our HashSet: " + set4);
+
+        if(set4.add("Blackberry"))
+            System.out.println("There is no such value in set, I am adding it ");
+        else System.out.println("There is already value in set, I am not adding");
+
+        //for loop is not applicable for Sets since we dont have .get() method
+
+
+        System.out.println("We are using Iterator to parse through Set: ");
+        //we can use for each loop as well
+        Iterator iterator = set4.iterator();
+        while(iterator.hasNext()) {
+
+            String currentElementInTheSet = (String)iterator.next();
+            //we are using casting for next(), because it returns Object type
+            System.out.println(currentElementInTheSet + " ");
+
+        }
+
+
+
     }
 }
