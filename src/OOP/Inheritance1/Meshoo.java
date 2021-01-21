d11a5efae621d7dfbc0c8882ba839861d0503d0c
Ridvan Cekmez <ridvanceko@gmail.com>
2021-01-20 18:56:53 -0700

    Inheritance

--- src/OOP/Inheritance1/Meshoo.java
+++ src/OOP/Inheritance1/Meshoo.java
@@ -1,25 +1,42 @@
 package OOP.Inheritance1;
 
+import OOP.Inheritance.Animal;
+import OOP.Inheritance.Cat;
+
 public class Meshoo extends Dog{
 
-
+   String name;
     // if the parent class has no no-argument constructor child class must crate the constructor
     // and implement the super class constructor.
 
     public Meshoo() {
            super("Meshoo", 1, 3, 10);
+           this.name = "Meshoo";
+        System.out.println("Test");
     }
 
     public Meshoo(String name, int height, int weight) {
-        super(name, 1, height, weight);
+        super(name, 1, height, weight); }
 
+
+    public void play(){
+        System.out.println(super.name + " is playing"); // i can say also this.name
     }
+
+    @Override
+    public Cat barking() {
+        System.out.println(this.name + " is barking");
+        return new Cat ();
+    }
+
 
     public static void main(String[] args) {
 
         Meshoo meshoo = new Meshoo();
         System.out.println(meshoo.name);
         // System.out.println(meshoo.weight) --> since the weight is private we can reach
+        System.out.println(meshoo.getWeight());
+        meshoo.play();
 
         System.out.println(meshoo.getWeight()); // -->  now we can get it
 
@@ -27,5 +44,7 @@ public class Meshoo extends Dog{
         System.out.println(newMeshoo.name);
         System.out.println(newMeshoo.age);
         System.out.println(newMeshoo.getWeight());
+
+        newMeshoo.barking();
     }
 }
