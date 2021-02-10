277de96338be110e037fbe38ca990b026e264c62
Ridvan Cekmez <ridvanceko@gmail.com>
2021-02-09 21:24:06 -0700

    Selenium-Xpath

--- src/test/java/selenium/locators/XpathAdv.java
+++ src/test/java/selenium/locators/XpathAdv.java
@@ -1,6 +1,7 @@
 package test.java.selenium.locators;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -21,8 +22,30 @@ public class XpathAdv {
 
         checkbox1.click();
 
-        checkbox1.isSelected();
+        boolean isChecked = checkbox1.isSelected();
+        Assert.assertTrue(isChecked);
+        System.out.println("Checkbox 1: " + isChecked);
 
 
     }
+
+    @Test
+    public void test2() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/checkboxes");
+
+        WebElement checkbox2 = driver.findElement
+                (By.xpath("//input[@type='checkbox']/input[1]/following-sibling::input"));
+
+        checkbox2.click();
+
+        boolean isChecked2 = checkbox2.isSelected();
+        Assert.assertTrue(isChecked2);
+        System.out.println("Checkbox 2: " + isChecked2);
+
+
+    }
+
+
 }
