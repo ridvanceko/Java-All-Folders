1df7c5284d67dd6f45973a84effc55fd7644e8a9
Ridvan Cekmez <ridvanceko@gmail.com>
2021-02-20 11:19:47 -0700

    running with XML files

---
+++ src/test/java/selenium/testng/Parameterization.java
@@ -0,0 +1,43 @@
+package test.java.selenium.testng;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class Parameterization {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setup() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+
+    }
+
+    @Parameters({"username", "password"})
+
+
+    @Test
+    public void parameterTest(String myUsername, String myPassword) {
+        driver.navigate().to("https://facebook.com");
+
+        Assert.assertTrue(driver.getCurrentUrl().contains("facebook"));
+
+        WebElement emailField = driver.findElement(By.id("email"));
+        WebElement passwordField = driver.findElement(By.id("pass"));
+        emailField.sendKeys(myUsername);
+        passwordField.sendKeys(myPassword);
+
+
+
+    }
+
+
+}
