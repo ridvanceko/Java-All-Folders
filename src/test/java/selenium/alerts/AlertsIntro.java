24b7cb918c2994c7551441cca6bbb3458fd17890
Ridvan Cekmez <ridvanceko@gmail.com>
2021-02-25 15:33:49 -0700

    types of Alerts

--- src/test/java/selenium/alerts/AlertsIntro.java
+++ src/test/java/selenium/alerts/AlertsIntro.java
@@ -1,10 +1,7 @@
 package test.java.selenium.alerts;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.BeforeMethod;
@@ -27,18 +24,18 @@ public class AlertsIntro {
     }
 
     @Test
-    public void alertsTest() {
+    public void alertsTest() throws InterruptedException {
 
         driver.get("https://the-internet.herokuapp.com/");
-        WebElement javaScriptAlert=driver.findElement(By.xpath("//a[text()='JavaScript Alerts']"));
+        WebElement javaScriptAlert = driver.findElement(By.xpath("//a[text()='JavaScript Alerts']"));
         actions.keyDown(Keys.SHIFT).click(javaScriptAlert).perform();
 
         String firstWindowHandle = driver.getWindowHandle();
         Set<String> allWindowHandles = driver.getWindowHandles();
 
         String secondWindowHandle = "";
-        for(String id: allWindowHandles) {
-            if(!id.equals(firstWindowHandle)) {
+        for (String id : allWindowHandles) {
+            if (!id.equals(firstWindowHandle)) {
                 secondWindowHandle = id;
             }
         }
@@ -46,12 +43,32 @@ public class AlertsIntro {
         driver.switchTo().window(secondWindowHandle);
 
         WebElement simpleAlert = driver.findElement(By.xpath("//button[@onclick='jsAlert()']"));
-        
+        simpleAlert.click();
 
+
+        // handles alert
         driver.switchTo().alert().accept();
         //other types of alerts
 //        driver.switchTo().alert().dismiss();
 //        driver.switchTo().alert().sendKeys();
+//
+//        Alert alert = driver.switchTo().alert();
+//        alert.accept();
 
+        // confirmation alert
+        WebElement confirmAlert = driver.findElement(By.xpath("//button[@onclick='jsConfirm()']"));
+        confirmAlert.click();
+        Thread.sleep(2000);
+        driver.switchTo().alert().dismiss();
+
+
+        // promp alert
+        WebElement promptAlert = driver.findElement(By.xpath("//button[@onclick='jsPrompt()']"));
+
+        promptAlert.click();
+        Thread.sleep(2000);
+        Alert promptAlertWindow = driver.switchTo().alert();
+        promptAlertWindow.sendKeys("Hello World");
+        Thread.sleep(2000);
     }
 }
